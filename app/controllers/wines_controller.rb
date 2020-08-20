9a01decb7625fea30a28f3cb05e7033cb85910e1
aaronfeingold <ajfeingold88@gmail.com>
2020-08-19 19:30:12 -0500

    third commit

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,2 +1,29 @@
 class WinesController < ApplicationController
+
+  def show
+    @wine = Wine.find(params[:id])
+  end
+
+  def index
+    @wines = Wine.all
+  end
+
+  def new
+    @wine = Wine.new
+    @wine.vineyards.build(name: "Vineyard 1")
+  end
+
+  def create
+    binding.pry
+    wine.create(wine_params)
+    redirect_to wines_path
+  end
+
+  private
+  def wine_params
+    params.require(:wine).permit(
+      :name,
+      vineyard_attributes: [ :name, :varietals ]
+    )
+  end
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -2,6 +2,8 @@ class Wine < ApplicationRecord
   has_many :varietals
   has_many :vineyards, through: :varietals
 
+  accepts_nested_attributes_for :vineyards
+
   scope :bottled, -> { where.not(bottled_date: nil) }
   scope :not_bottled, -> { where(bottled_date: nil) }
   scope :recent_bottled_wines, -> { self.bottled.where("bottled_date > ?", 5.years.ago) }
